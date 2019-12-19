84920fb8f1a214600e63d356f3ecc25790bd374a
Sabatino Matteo <sabatinomatteo.98@gmail.com>
2019-12-19 08:18:38 +0000

    Update Boruvka.cpp

--- src/Boruvka.cpp
+++ src/Boruvka.cpp
@@ -3,7 +3,7 @@
 using namespace Rcpp;
 
 
-// [[Rcpp::export]]
+// [[Rcpp::export(.dfs3)]]
 void dfs3 (int x, NumericMatrix g, NumericVector vis1, int comp, int & cont){
     int n1=g.nrow(); //numero di nodi
     vis1(x)=1; //setto come visitato il nodo che ho passato come radice di dfs
@@ -16,7 +16,7 @@ void dfs3 (int x, NumericMatrix g, NumericVector vis1, int comp, int & cont){
     }
 }
 
-// [[Rcpp::export]]
+// [[Rcpp::export(.find1)]]
 int find1(NumericMatrix subset, int i){
     if(subset(i,0)!=i){ //ricerco l'antenato del nodo i
         subset(i,0)=find1(subset,subset(i,0));
@@ -24,18 +24,18 @@ int find1(NumericMatrix subset, int i){
     return subset(i,0);
 }
 
-// [[Rcpp::export]]
+// [[Rcpp::export(.Union)]]
 void Union(NumericMatrix subset, int x, int y){
-    int xroot=find1(subset,x);//ricerco antenato di x
-    int yroot=find1(subset,y);// ricerco antenato di y
-    if(subset(xroot,1)<subset(yroot,1)){//se il rank del nodo antenato di x è minore del rank del nodo antenato di y
-        subset(xroot,0)=yroot;//metto come parente del nodo antenato di x il nodo antenato di y
-        
+    int xroot=find1(subset,x); //ricerco antenato di x
+    int yroot=find1(subset,y); // ricerco antenato di y
+    if(subset(xroot,1)<subset(yroot,1)){ //se il rank del nodo antenato di x è minore del rank del nodo antenato di y
+        subset(xroot,0)=yroot; //metto come parente del nodo antenato di x il nodo antenato di y
     }else{
         if(subset(xroot,1)>subset(yroot,1)){
-            subset(yroot,0)=xroot;//altrimenti il viceversa
-        }else{
-            if(subset(xroot,1)==subset(yroot,1)){//se sono uguali ne posso prendere uno a caso
+            subset(yroot,0)=xroot; //altrimenti il viceversa
+        }
+        else{
+            if(subset(xroot,1)==subset(yroot,1)){ //se sono uguali ne posso prendere uno a caso
                 subset(yroot,0)=xroot;
                 subset(xroot,1)++;
             }
@@ -44,14 +44,14 @@ void Union(NumericMatrix subset, int x, int y){
 }
 
 
-// [[Rcpp::export]]
+// [[Rcpp::export(.boruvka)]]
 List boruvka (NumericMatrix graph, NumericMatrix adj, int comp){
     int v=graph.nrow();
-    NumericMatrix mst(v,v);//matrice albero MST
+    NumericMatrix mst(v,v); //matrice albero MST
     int e=0; //contatore archi
     int i,j;
-    int k=0;//indice per scorrere la matrice edge
-    int start;//indice di partenza per dfs a contare il numero di nodi all'interno della componente
+    int k=0; //indice per scorrere la matrice edge
+    int start;//indice di partenza per dfs a contare il numero di nodi presenti nella componente
     List returned;
     NumericVector vis(v);
     for(i=0;i<v;i++){
@@ -66,8 +66,8 @@ List boruvka (NumericMatrix graph, NumericMatrix adj, int comp){
              }
          }
     }
-    k=start;//cosi inizio a riempire vettore edge dall'indice giusto in caso di più componenti
-    int nodi=1;//contatore nodi che verrà modificato con dfs 3
+    k=start; //cosi inizio a riempire vettore edge dall'indice giusto in caso di più componenti
+    int nodi=1; //contatore nodi che verrà modificato con dfs 3
     for(i=start;i<v;i++){
         dfs3(i,adj,vis,comp,nodi); //conto il numero di nodi della componente
     }
@@ -89,10 +89,16 @@ List boruvka (NumericMatrix graph, NumericMatrix adj, int comp){
     CharacterVector rn= rownames(graph);
     rownames(mst)=rn;
     colnames(mst)=rn;
-    NumericMatrix edge (v,3); //matrice contenente gli archi
+    int x;
+    if(v<e){
+        x=e; //se il numero di archi è maggiore del numero di nodi allora le righe della tabella edge dovrà essere uguale al numero di archi
+    }else{
+        x=v;
+    }
+    NumericMatrix edge (x,3); //matrice contenente gli archi
     for(i=0;i<v;i++){
         for(j=0;j<v;j++){
-            if((graph(i,j)>0)&(adj(i,j)==comp)){
+            if(adj(i,j)==comp){
                 edge(k,0)=i;//partenza arco
                 edge(k,1)=j;//destinazione arco
                 edge(k,2)=graph(i,j);//peso arco
@@ -100,49 +106,54 @@ List boruvka (NumericMatrix graph, NumericMatrix adj, int comp){
             }
         }
     }
-    NumericMatrix subset (v,2);//matrice parenti e rank
-    NumericVector cheapest(v);//matrice percorsi minimi
+    NumericMatrix subset (v,2); //matrice parenti e rank
+    NumericVector cheapest(v); //matrice valore minimo per giungere al nodo
     for(i=start;i<v;i++){
         subset(i,0)=i;//parente nodo i
         subset(i,1)=0;//rank nodo i
-        cheapest(i)=-1;//peso più basso dal nodo i
+        cheapest(i)=-1;//arco con peso più basso per raggiungere un nodo da nodo partenza
     }
-    int contarchi=0; //contatore archi nell'albero
+    int contarchi=nodi; //contatore archi nell'albero
     int mstweight=0; //peso albero
     int set1,set2;
-    while(contarchi<nodi-1){
-        for(i=0;i<nodi;i++){
+    while(contarchi>1){
+        for(i=start;i<v;i++){
             cheapest(i)=-1;
         }
-        for(i=0;i<v;i++){
-            set1=find1(subset,edge(i,0));//trovo l'antenato del nodo da cui parte l'arco i
-            set2=find1(subset,edge(i,1));//trovo l'antenato del nodo a cui arriva l'arco i
+        
+        for(i=0;i<x;i++){
+            set1=find1(subset,edge(i,0)); //trovo l'antenato del nodo da cui parte l'arco i
+            set2=find1(subset,edge(i,1)); //trovo l'antenato del nodo a cui arriva l'arco i
             
-            if(set1!=set2){ //se non hanno antenato comune
-                if((cheapest(set1)==-1)|(edge(cheapest(set1),2)>edge(i,2))){//controllo se non ho ancora preso in considerazione quell'arco o se l'arco che ho preso in considerazione ha peso minore dell'arco che avevo preso in considerazione precentemente
-                    cheapest(set1)=i;//se è cosi a cheapest di indice i associo i
+            if(set1==set2){// se non hanno antenato comune
+                continue;
+            }
+            if((cheapest(set1)==-1)|(edge(cheapest(set1),2)>edge(i,2))){ //controllo se non ho ancora preso in considerazione quell'arco o se l'arco che ho preso in considerazione ha peso minore dell'arco che avevo preso in considerazione precentemente
+                    cheapest(set1)=i; //se è cosi a cheapest di indice i associo i
                 }
                 if((cheapest(set2)==-1)|(edge(cheapest(set2),2)>edge(i,2))){
                     cheapest(set2)=i;
                 }
+            
             }
-        }
+        
         for(i=0;i<v;i++){
             if(cheapest(i)!=-1){
                 set1=find1(subset,edge(cheapest(i),0));//trovo l'antenato del nodo da cui parte l'arco i
                 set2=find1(subset,edge(cheapest(i),1));//trovo l'antenato del nodo a cui arriva l'arco i
                
-                if(set1!=set2){// se non hanno antenato comune
+                if(set1==set2){
+                    continue;
+                }// se non hanno antenato comune
                     mstweight+=edge(cheapest(i),2); //incremento il contatore del peso totale dell'albero
                     mst(edge(cheapest(i),0),edge(cheapest(i),1))=1;//alla radice di adiacenza dell'albero aggiungo un arco
-                    Union(subset,set1,set2);//aggiungo l'arco effettivo all'albero
-                    contarchi ++;//incremento il contatore degli archi
+                    Union(subset,set1,set2); //aggiungo l'arco effettivo all'albero
+                    contarchi --; //incremento il contatore degli archi
             }
         }
     }
-}
-    returned=List::create(mst,mstweight);//ritorno la lista con il peso totale minimo dell'albero e la matrice di adiacenza di MST
-return returned;
+    returned=List::create(mst,mstweight); //ritorno la lista con il peso totale minimo dell'albero e la matrice di adiacenza di MST
+    return returned;
 }
 
 
@@ -160,8 +171,8 @@ List Boruvka(NumericMatrix matrixpeso, NumericMatrix adjacencymatrix){
             support(i,j)=adjacencymatrix(i,j); //inizializzo support con i valori della matrice di adiacenza
         }
     }
-    //rownames(support)=rn;
-    //colnames(support)=cn;
+    rownames(support)=rn;
+    colnames(support)=cn;
     int n=component1(matrixpeso, support);//conto il numero di componenti del grafo e mi segno nella matrice di adiacenza a quale componente sono associati gli archi
     List costi; //dichiaro lista costi contente costo minimo e matrice adiacenza albero di ogni componente
     if(n==0){ //se n è uguale a 0 non ho componenti e nemmeno archi
